f68f5f37304a1d57a524d3f397b733d67552c4f0
Jax0312 <jaxonmoh05@gmail.com>
2022-03-13 20:09:24 +0800

    Reimplemented CORS settings with rocket_cors

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-#![feature(in_band_lifetimes)]
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 use std::{collections::HashMap};
 use rocket::serde::json::Json;
-use rocket::http::Header;
-use rocket::{Request, Response};
+use rocket::http::Method;
 use serde::Serialize;
-use rocket::fairing::{Fairing, Info, Kind};
+use std::error::Error;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 mod map_simple_to_traditional;
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
 
 #[derive(Serialize)]
 pub struct Payload {
@@ -37,8 +18,23 @@ fn simplified_to_traditional(data: &str) -> Json<Payload>{
     Json(Payload{data: map_simple_to_traditional::generate_map(data.trim().split("").collect::<Vec<&str>>())})
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![simplified_to_traditional]).attach(CORS)
+#[rocket::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true)
+        .to_cors()?;
+
+    rocket::build().mount("/", routes![simplified_to_traditional]).attach(cors).launch().await?;
+
+    Ok(())
+
 }
 
